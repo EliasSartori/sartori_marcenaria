1cbb57a406fe5da8b302484bd2438bd8e4deaf14
Elias Sartori <elias.sartori@gmail.com>
2018-05-25 21:42:12 -0300

    Products Fixes

--- app/controllers/backoffice/products_controller.rb
+++ app/controllers/backoffice/products_controller.rb
@@ -1,4 +1,7 @@
 class Backoffice::ProductsController < BackofficeController
+
+  before_action :set_product, only: [:edit, :update, :destroy]
+
   def index
     @products = Product.all
   end
@@ -17,21 +20,31 @@ class Backoffice::ProductsController < BackofficeController
   end
 
   def edit
-
+    
   end
 
   def update
-
+    if @product.update(product_params)
+      redirect_to backoffice_products_path, notice: "Produto atualizado!"
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    if @product.destroy
+      redirect_to backoffice_products_path, notice: "Produto deletado!"
+    end
   end
 
   private
 
-  def product_params
-    params.require(:product).permit(:description, :quantity, :price)
-  end  
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:description, :quantity, :price)
+    end  
 
 end
